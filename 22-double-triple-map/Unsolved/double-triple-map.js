e367545e1217d4c6e782c55a752ff9867c43ceae
chrismuto <chrismuto@gmail.com>
2022-04-06 12:26:04 -0700

    solved array intersection algorithm

--- 22-double-triple-map/Unsolved/double-triple-map.js
+++ 22-double-triple-map/Unsolved/double-triple-map.js
@@ -2,4 +2,13 @@
 // If a element in the original array is even, the element at the same index in the new array should be double the original element
 // If an element in the original array is odd, the element at the same index of the new array should be triple the original element
 
-var doubleTripleMap = function(arr) {};
+var doubleTripleMap = function(arr) {
+    const finalArr = arr.map(num => {
+        if (num % 2 === 0) {
+           return num * 2;
+        } else {
+           return num * 3;
+        }
+    })
+    return finalArr;
+};
--- 23-array-intersection/Unsolved/array-intersection.js
+++ 23-array-intersection/Unsolved/array-intersection.js
@@ -1,4 +1,16 @@
 // Write code to create a function that accepts two arrays of numbers
 // Return a new array containing the intersecting elements of the arrays
 
-var arrayIntersection = function(arr1, arr2) {};
+var arrayIntersection = function(arr1, arr2) {
+    var finalArray = [];
+    for (i = 0; i < arr1.length; i++) {
+        for (j = 0; j < arr2.length; j++) {
+            if (arr1[i] === arr2[j]) {
+                finalArray.push(arr1[i]);
+                j = arr2.length -1;
+            }
+        }
+    }
+    console.log(finalArray);
+    return finalArray;
+};
