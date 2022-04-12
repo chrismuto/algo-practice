516948594486e50e7bfd9308a640b9493f8fde60
chrismuto <chrismuto@gmail.com>
2022-04-11 22:46:57 -0700

    solved rotation point

--- 28-reverse-no-built-in/Unsolved/reverse-no-built-in.js
+++ 28-reverse-no-built-in/Unsolved/reverse-no-built-in.js
@@ -2,10 +2,9 @@
 // You may NOT use the built-in reverse method
 
 const reverse = (str) => {
-    var splitStr = str.split('');
     var revStr = [];
-    for (i = 0; i < splitStr.length; i ++) {
-        revStr[i] = splitStr[splitStr.length - 1 - i];
+    for (i = 0; i < str.length; i ++) {
+        revStr[i] = str[str.length - 1 - i];
     }
     return revStr.join('');
 };
--- 29-rotation-point/Unsolved/rotation-point.js
+++ 29-rotation-point/Unsolved/rotation-point.js
@@ -1,3 +1,20 @@
 // Write a function that takes in an array of sorted strings and returns the index of its rotation point if it has one, else return -1
 
-var rotationPoint = function(words) {};
+var rotationPoint = function(words) {
+    for (i = 0; i < words.length; i++) {
+        var element = words[i];
+        var element2 = words[i+1];
+        var elArray = [element, element2];
+        var wordArray = [element, element2];
+        var sortTestArray = wordArray.sort();
+        console.log(elArray);
+        console.log(sortTestArray);
+        if (i + 1 === undefined) {
+            return -1;
+        } else if (elArray[0] !== sortTestArray[0]) {
+            console.log(i+1);
+            return i+1;
+        }
+    }
+    return -1;
+};
