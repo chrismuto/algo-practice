e07e84f8acc3b0d2db4bd1fcbcbbeed31937cd0e
chrismuto <chrismuto@gmail.com>
2022-04-12 11:44:21 -0700

    solved reverse-in-place algorithm

--- 30-reverse-in-place/Unsolved/reverse-in-place.js
+++ 30-reverse-in-place/Unsolved/reverse-in-place.js
@@ -1,4 +1,16 @@
 // Write code to return a new string with all of the characters in `str` reversed.
 // You may NOT use the built-in reverse method
 
-const reverseInPlace = (arr) => {};
+const reverseInPlace = (arr) => {
+    console.log(arr);
+    let i = 0;
+    let count = arr.length - 1;
+    while (count > i) {
+        const placeholder = arr[i];
+        arr[i] = arr[count];
+        arr[count] = placeholder;
+        i++;
+        count--;
+    }
+    return arr;
+};
