6a93f92d58c87fb91bde1c4b21ec7afe82f9068c
chrismuto <chrismuto@gmail.com>
2022-04-12 14:45:29 -0700

    solved min increment for unique algorithm

--- 32-min-increment-for-unique/Unsolved/min-increment-for-unique.js
+++ 32-min-increment-for-unique/Unsolved/min-increment-for-unique.js
@@ -1,3 +1,18 @@
 // Write a function to take an array of integers and return the minimum number of increments required across the array to ensure that every number is unique
 
-const minIncrement = function(nums) {};
+const minIncrement = function(nums) {
+    let total = 0;
+    for (i = 0; i < nums.length; i++) {
+        console.log(nums[i]);
+        for (j = i+1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                nums[j]++;
+                total++;
+                i = 0;
+                j = i+1;
+                console.log(total);
+            }
+        }
+    }
+    return total;
+};
