d9a756cff8bef2ee5e54a40e98b7864d0591b4f4
chrismuto <chrismuto@gmail.com>
2022-04-13 11:28:05 -0700

    solved maximum continuous subarray algorithm

--- 36-smallest-difference/01-maximum-continuous-subarray/Unsolved/maximum-continuous-subarray.js
+++ 36-smallest-difference/01-maximum-continuous-subarray/Unsolved/maximum-continuous-subarray.js
@@ -1,3 +1,20 @@
+// Write your solution here
 const maxSumArray = function(numbers) {
-  // Write your solution here
+  let total2 = numbers[0];
+  for (i = 0; i < numbers.length; i++) {
+    let total1;
+    total1 = numbers[i];
+    for (j = i+1; j <= numbers.length; j++) {
+      if (numbers[j] === undefined) {
+        continue;
+      }
+      if (total1 < total1 + numbers[j]);
+      total1 = total1 + numbers[j]
+      console.log(total1);
+      if (total1 > total2) {
+        total2 = total1
+      }
+    }
+  }
+  return total2;
 };
