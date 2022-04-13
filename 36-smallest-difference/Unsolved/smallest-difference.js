48367f54662da9b57cb72cd6e3d18e48f8685540
chrismuto <chrismuto@gmail.com>
2022-04-13 10:43:48 -0700

    smallest difference solved

--- 36-smallest-difference/Unsolved/smallest-difference.js
+++ 36-smallest-difference/Unsolved/smallest-difference.js
@@ -3,4 +3,21 @@
 
 // TODO: Solve the function below:
 
-var smallestDifference = function (arr1, arr2) {};
+var smallestDifference = function (arr1, arr2) {
+    let difference = Infinity;
+    let final = [];
+    let target1;
+    let target2;
+    for (i = 0; i < arr1.length; i++) {
+        for (j = 0; j < arr2.length; j++) {
+            console.log(difference);
+            if (Math.abs(arr1[i] - arr2[j]) < difference) {
+                difference = Math.abs(arr1[i] - arr2[j]);
+                target1 = arr1[i];
+                target2 = arr2[j];
+            }
+        }
+        final = [target1, target2]
+    }
+    return final;
+};
